1933256f7af41b9d2ed07d646e4679ee831edb70
TracyChou2020 <62250909+TracyChou2020@users.noreply.github.com>
2021-01-07 18:23:35 +0330

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$hhh="";
+?>
+
+
+<html>
+  <body>
+      <form method="post" name="form_decryption" class="inline-div">
+          <br>
+              <input type="submit" name="submit"  id="submit" class="myButton" value="Decrypt"/>
+              <textarea id="rightTextarea" name="rightTextarea" class="inline-txtarea" placeholder="right Text area">fgfdgfgsf</textarea>
+              <!-- <input type="submit" name="add" value="ADD"/> -->
+          </form>
+
+          <!-- <div class="content"><br><br>
+          </div> -->
+      </div>
+    </body>
+</html>
